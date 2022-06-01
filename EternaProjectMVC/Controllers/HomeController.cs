940fa11788566752047c4b0ef895abfa5b9e1b36
onursmlv <onurtis@code.edu.az>
2022-06-01 10:11:41 +0400

    init

---
+++ EternaProjectMVC/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EternaProjectMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
